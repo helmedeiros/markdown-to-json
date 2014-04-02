f79226b9fb3558bfcd7e50c7b8d2fa8f33ffd391
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-04-02 14:21:38 -0300

    Is abble to process tables with thead

--- test/specs/core.test.js
+++ test/specs/core.test.js
@@ -1,4 +1,4 @@
-/* Should gett the a row headings */
+/* Should get the row's headings */
 test('should return headings whithout thead', function(){
 
   var table = $(
@@ -19,6 +19,42 @@ test('should return headings whithout thead', function(){
             '<td></td>' +
             '<td>7</td>' +
           '</tr>' +
+      '</table>'
+    );
+
+  var converter = new TableConverter();
+  var headings = converter.getHeadings(table);
+  debugger;
+  var expected = ["numero","Historias tarefas","Back","Front","SEO","QA"];
+  deepEqual(headings, expected);
+
+});
+
+/* Should get the row's headings */
+test('should return headings whith thead', function(){
+
+  var table = $(
+      '<table id="test-table">' +
+        '<thead>' +
+          '<tr>' +
+            '<th>numero</th>' +
+            '<th>Historias tarefas</th>' +
+            '<th>Back</th>' +
+            '<th>Front</th>' +
+            '<th>SEO</th>' +
+            '<th>QA</th>' +
+          '</tr>' +
+        '</thead>' +
+        '<tbody>' +
+          '<tr>' +
+            '<td>35</td>' +
+            '<td>Publicação flip AN e SOL</td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td></td>' +
+            '<td>7</td>' +
+          '</tr>' +
+        '</tbody>' + 
       '</table>'
     );
 
